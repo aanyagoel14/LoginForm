2dc74e23695ef347815ca6b26ddf6244ba3572f6
Aanya Goel <131501514+aanyagoel14@users.noreply.github.com>
2025-05-16 20:03:33 +0000

    Added validate page with if statement.
    added hardcoded username and password.

--- login.php
+++ login.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Login Page</title>
+    <title>Login Form</title>
   </head>
     <body>
 
 <h2>Login</h2>
 
-    <form action="login_process.php" method="post">
+    <form action="/validate.php" method="post">
         <label for="username">Username:</label><br>
         <input type="text" id="username" name="username"><br><br>
 
